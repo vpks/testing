74563d913d4f9f4e84a157e51578cdbb7bb372d9
Venkadesh Prasad <venkadeshprasad@Venkadeshs-MacBook-Air.local>
2024-12-23 10:59:44 +0530

    final  commit

--- express.js
+++ express.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
+dotenv.config({ path: "./eVaraibles.env" });
 const app = express();
 
 app.get("/", (req, res, next) => {
   res.status(200).send("hello");
 });
-
-app.listen(8000, () => {
+const port = process.env.process_port || 3000;
+app.listen(port, () => {
   console.log("server started");
 });
