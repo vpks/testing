deeeec7cdad662cb4701e815fc925424859a0af1
Venkadesh Prasad <venkadeshprasad@Venkadeshs-MacBook-Air.local>
2024-12-28 18:02:01 +0530

    final6  commit

--- express.js
+++ express.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const clusters = require("cluster");
+const multer = require("multer");
+const path = require("path");
+
+const storages = multer.diskStorage({
+  destination: path.join(__dirname, "files"),
+  filename: (req, file, res) => {
+    res(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storages,
+});
 
 dotenv.config({ path: "./eVaraibles.env" });
 const app = express();
 
 app.get("/", (req, res, next) => {
   res.status(200).send("<h1>hello sundaram.</h1>");
+});
+app.post("/", upload.single("file"), (req, res, next) => {
+  res.status(201).send("Files uploaded successfully");
 });
 const port = process.env.process_port || 3000;
 app.listen(port, () => {
