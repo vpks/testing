0f1d6fa8141326e0e36d2cf52316cc700a5d8895
Venkadesh Prasad <venkadeshprasad@Venkadeshs-MacBook-Air.local>
2025-05-17 20:06:43 +0530

    final10  commit

--- express.js
+++ express.js
@@ -24,7 +24,7 @@ app.get("/", (req, res, next) => {
   res.status(200).send("<h1>hello sundaram.</h1>");
 });
 app.post("/", upload.single("file"), (req, res, next) => {
-  res.status(201).send(req.file.size);
+  res.status(201).send(req.file.filename);
 });
 const port = process.env.process_port || 3000;
 app.listen(port, () => {
