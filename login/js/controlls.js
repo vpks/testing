bc06c8db0bcd2ed1ad64cccf487f3a9f8f029f81
Venkadesh Prasad <venkadeshprasad@Venkadeshs-MacBook-Air.local>
2025-05-28 00:05:03 +0530

    final35  commit

--- login/js/controlls.js
+++ login/js/controlls.js
@@ -3,6 +3,7 @@ const password = document.getElementById("password").textContent;
 
 document.querySelector(".button").addEventListener("submit", (e) => {
   e.preventDefault();
+  console.log("cliked");
   click();
 });
 
