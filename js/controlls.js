9faad3d7c069700a6154e318ca72d16303ab0bc5
Venkadesh Prasad <venkadeshprasad@Venkadeshs-MacBook-Air.local>
2025-05-27 23:37:00 +0530

    final29  commit

--- js/controlls.js
+++ js/controlls.js
@@ -1,7 +1,7 @@
 const email = document.getElementById("email").textContent;
 const password = document.getElementById("password").textContent;
 
-document.querySelector(".button").addEventListener("Submit", (e) => {
+document.querySelector(".button").addEventListener("submit", (e) => {
   e.preventDefault();
   click();
 });
