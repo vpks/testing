97aabeb2dc69f71f817ffb0cd3bafcfd7d6dd06a
Venkadesh Prasad <venkadeshprasad@Venkadeshs-MacBook-Air.local>
2025-05-29 22:15:17 +0530

    final43  commit

--- login/js/controlls.js
+++ login/js/controlls.js
@@ -1,5 +1,5 @@
 document.querySelector("#f").addEventListener("click", (e) => {
-  const email = document.getElementById("email").value;
+  const email = document.getElementById("email1").value;
   const password = document.getElementById("password").value;
   e.preventDefault();
   alert(email);
