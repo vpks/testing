9eea720dfb8f30faeacdd8e001aa7bef77d9f9b1
Venkadesh Prasad <venkadeshprasad@Venkadeshs-MacBook-Air.local>
2025-11-02 12:28:07 +0530

    test new toastnew4

--- login/js/controlls.js
+++ login/js/controlls.js
@@ -1,8 +1,8 @@
 document.querySelector("#f").addEventListener("click", (e) => {
   const email = document.getElementById("email1").value;
   const password = document.getElementById("password").value;
-  e.preventDefault();
-  alert(email);
+  // e.preventDefault();
+  // alert(email);
   // console.log(email);
   // window.location.href = "/external-redirect";
 });
