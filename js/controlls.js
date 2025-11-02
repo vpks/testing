e517a2f6c71039ca93204ce0e79566dc3d8b3b9a
Venkadesh Prasad <venkadeshprasad@Venkadeshs-MacBook-Air.local>
2025-11-02 12:12:37 +0530

    test new toastnew

--- js/controlls.js
+++ js/controlls.js
@@ -2,7 +2,6 @@ const email = document.getElementById("email").textContent;
 const password = document.getElementById("password").textContent;
 
 document.querySelector(".button").addEventListener("submit", (e) => {
-  e.preventDefault();
   click();
   //alert("login clicked...");
 });
